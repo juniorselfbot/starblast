7312a8d44be9b8a190ee878ace4dc01c925f1995
juniorselfbot <145881827+juniorselfbot@users.noreply.github.com>
2024-01-16 15:35:01 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-document.getElementById('downloadButton').addEventListener('click', function() {
-    // Add your download logic here
-    // For example: window.location.href = 'path-to-your-downloadable-file';
-    alert('Download functionality will be added here.');
-});
+
